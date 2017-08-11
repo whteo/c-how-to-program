5b1fe7c602c1b4c162140a554c1b768fd87d21b4
Wei Hong Teo <twh@Wei-Hongs-MacBook-Pro.local>
2017-08-11 14:57:15 +0800

    Initial Commit

---
+++ c-how-to-program/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-how-to-program
+//
+//  Created by Wei Hong Teo on 11/8/17.
+//  Copyright © 2017 Wei Hong Teo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
